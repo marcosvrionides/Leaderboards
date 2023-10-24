a4c2372029ec43eb4e87c19d49e47d9e74459797
vrionides2000 <vrionides2000@gmail.com>
2023-10-24 14:31:35 +0100

    fcm messages when update available

--- App.tsx
+++ App.tsx
@@ -1,4 +1,13 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import {
+  PermissionsAndroid,
+  AppRegistry,
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Linking,
+} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './src/Home';
@@ -6,13 +15,31 @@ import AddGame from './src/AddGame';
 import SelectLeaderboard from './src/SelectLeaderboard';
 import {GAMBannerAd, BannerAdSize} from 'react-native-google-mobile-ads';
 import NewLeaderboardForm from './src/NewLeaderboardForm';
-import VersionCheck from 'react-native-version-check';
+import messaging from '@react-native-firebase/messaging';
+import colors from './src/Colours';
 
 const App = () => {
+  const [updateAvailable, setUpdateAvailable] = useState(false);
+
+  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+
+  // Register background handler
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
+    setUpdateAvailable(true);
+  });
+
   const Stack = createNativeStackNavigator();
 
   const onAdFailedToLoad = error => {
     console.log('error loading ad', error.message);
+  };
+
+  const openPlayStore = () => {
+    Linking.openURL(
+      'https://play.google.com/store/apps/details?id=com.backgammon_leaderboards&pcampaignid=web_share',
+    ).catch(err => {
+      console.error('Could not open the Play Store', err);
+    });
   };
 
   return (
@@ -22,6 +49,16 @@ const App = () => {
         sizes={[BannerAdSize.ANCHORED_ADAPTIVE_BANNER]}
         onAdFailedToLoad={onAdFailedToLoad}
       />
+      {!updateAvailable && (
+        <View style={styles.updateContainer}>
+          <Text>Update available for the app</Text>
+          <Button
+            title="Update Now"
+            color={colors.accent}
+            onPress={() => openPlayStore()}
+          />
+        </View>
+      )}
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
@@ -49,5 +86,17 @@ const App = () => {
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  updateContainer: {
+    height: 100,
+    backgroundColor: colors.background,
+    marginTop: 10,
+    elevation: 7,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10,
+  },
+});
 
 export default App;

718c64754d24f9433e3ac58c65c819c4867aa98b
vrionides2000 <vrionides2000@gmail.com>
2023-11-14 11:26:12 +0000

    add or remove multiple players

--- src/AddGame.js
+++ src/AddGame.js
@@ -8,6 +8,7 @@ import {
   TextInput,
   Image,
   BackHandler,
+  ScrollView,
 } from 'react-native';
 import React, {useState, useEffect, useRef} from 'react';
 import colours from './Colours';
@@ -23,22 +24,14 @@ import messaging from '@react-native-firebase/messaging';
 const AddGame = ({navigation, route}) => {
   const leaderboard = route.params.leaderboard;
 
-  const [player1Name, setPlayer1Name] = useState('');
-  const [player2Name, setPlayer2Name] = useState('');
-  const [player1GamesWon, setPlayer1GamesWon] = useState('');
-  const [player2GamesWon, setPlayer2GamesWon] = useState('');
+  const [numberOfPlayers, setNumberOfPlayers] = useState(2);
+  const [playerNames, setPlayerNames] = useState([]);
+  const [playersGamesWon, setPlayersGamesWon] = useState([]);
   const [gameNote, setGameNote] = useState();
   const [gameMedia, setGameMedia] = useState();
-  const [playerNames, setPlayerNames] = useState([]);
+  const [playerNamesSuggestions, setPlayerNamesSuggestions] = useState([]);
   const [filteredPlayer1Names, setFilteredPlayer1Names] = useState([]);
-  const [filteredPlayer2Names, setFilteredPlayer2Names] = useState([]);
-
-  const player1NameInputRef = useRef(null);
-  const player2NameInputRef = useRef(null);
-  const player1WinsInputRef = useRef(null);
-  const player2WinsInputRef = useRef(null);
-  const noteInputRef = useRef(null);
-  const passwordInputRef = useRef(null);
+  const [filteredPlayersNames, setFilteredPlayersNames] = useState([[]]);
 
   const [leaderboardPasswordInput, setLeaderboardPasswordInput] = useState('');
   const [leaderboardPassword, setLeaderboardPassword] = useState('');
@@ -51,28 +44,28 @@ const AddGame = ({navigation, route}) => {
     );
 
     leaderboard_ref.on('value', snapshot => {
-      const temp_playerNames = [];
+      const temp_playerNamesSuggestions = [];
 
       snapshot.forEach(childSnapshot => {
         const player1Name = childSnapshot.val().player_1_name;
         const player2Name = childSnapshot.val().player_2_name;
 
         if (
-          !temp_playerNames.includes(player1Name) &&
+          !temp_playerNamesSuggestions.includes(player1Name) &&
           player1Name !== undefined
         ) {
-          temp_playerNames.push(player1Name);
+          temp_playerNamesSuggestions.push(player1Name);
         }
 
         if (
-          !temp_playerNames.includes(player2Name) &&
+          !temp_playerNamesSuggestions.includes(player2Name) &&
           player2Name !== undefined
         ) {
-          temp_playerNames.push(player2Name);
+          temp_playerNamesSuggestions.push(player2Name);
         }
       });
 
-      setPlayerNames(temp_playerNames);
+      setPlayerNamesSuggestions(temp_playerNamesSuggestions);
     });
 
     leaderboard_password_ref.on('value', snapshot => {
@@ -82,24 +75,25 @@ const AddGame = ({navigation, route}) => {
     });
   }, []);
 
-  const filterPlayerNames = (player, inputText) => {
+  const filterPlayerNamesSuggestions = (player, inputText) => {
     if (inputText.length === 0) {
-      player === 1 ? setFilteredPlayer1Names([]) : setFilteredPlayer2Names([]);
+      setFilteredPlayer1Names([]);
+      temp = [...filteredPlayersNames];
+      temp[player - 1] = [];
+      setFilteredPlayersNames(temp);
       return;
     }
 
-    const filteredNames = playerNames.filter(
+    const filteredNames = playerNamesSuggestions.filter(
       name => name.toLowerCase().includes(inputText), // Convert each name to lowercase before comparison
     );
 
-    if (player === 1) {
-      setFilteredPlayer1Names(filteredNames);
-      setFilteredPlayer2Names([]);
-    } else {
-      setFilteredPlayer2Names(filteredNames);
-      setFilteredPlayer1Names([]);
-    }
+    temp = [...filteredPlayersNames];
+    temp[player - 1] = [filteredNames];
+    setFilteredPlayersNames(temp);
+    setFilteredPlayer1Names(filteredNames);
   };
+  console.log(filteredPlayersNames);
 
   const saveGame = () => {
     if (
@@ -194,38 +188,12 @@ const AddGame = ({navigation, route}) => {
     );
   };
 
-  const handleSuggestionPress = (suggestion, ref) => {
-    switch (ref) {
-      case 1:
-        setPlayer1Name(suggestion);
-        setFilteredPlayer1Names([]);
-        break;
-      case 2:
-        setPlayer2Name(suggestion);
-        setFilteredPlayer2Names([]);
-        break;
-    }
-    handleSwitchInputRef(ref);
-  };
-
-  const handleSwitchInputRef = ref => {
-    switch (ref) {
-      case 1:
-        player1WinsInputRef.current.focus();
-        break;
-      case 2:
-        player2WinsInputRef.current.focus();
-        break;
-      case 3:
-        player2NameInputRef.current.focus();
-        break;
-      case 4:
-        noteInputRef.current.focus();
-        break;
-      case 5:
-        passwordInputRef.current.focus();
-        break;
-    }
+  const handleSuggestionPress = (suggestion, player) => {
+    temp = [...playerNames];
+    temp[player - 1] = suggestion;
+    setPlayerNames(temp);
+    setFilteredPlayer1Names([]);
+    setFilteredPlayersNames([]);
   };
 
   useEffect(() => {
@@ -241,6 +209,65 @@ const AddGame = ({navigation, route}) => {
 
     return () => backHandler.remove();
   }, []);
+
+  const players = [];
+  for (let player = 1; player <= numberOfPlayers; player++) {
+    players.push(
+      <View key={player}>
+        <Text>Player {player}:</Text>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+          <TextInput
+            style={styles.formInput}
+            placeholder="Name"
+            placeholderTextColor={colours.light_text}
+            value={playerNames[player - 1]}
+            onChangeText={text => {
+              temp = [...playerNames];
+              temp[player - 1] = text;
+              setPlayerNames(temp);
+              filterPlayerNamesSuggestions(1, text.toLocaleLowerCase());
+            }}
+            maxLength={30}
+          />
+          <TextInput
+            style={styles.formInput}
+            placeholder="Games Won"
+            placeholderTextColor={colours.light_text}
+            value={playersGamesWon[player - 1]}
+            onChangeText={number => {
+              temp = [...playersGamesWon];
+              temp[player - 1] = number;
+              setPlayersGamesWon(temp);
+            }}
+            keyboardType="numeric"
+            maxLength={4}
+          />
+        </View>
+        {filteredPlayer1Names.length > 0 && (
+          <FlatList
+            keyboardShouldPersistTaps={'handled'}
+            style={styles.searchedUsersList}
+            data={filteredPlayer1Names.slice(0, 3)}
+            keyExtractor={item => item}
+            renderItem={({item}) => (
+              <TouchableOpacity
+                style={styles.searchedUserContainer}
+                onPress={() => {
+                  handleSuggestionPress(item, player);
+                }}>
+                <Text style={styles.searchedUserText}>{item}</Text>
+              </TouchableOpacity>
+            )}
+          />
+        )}
+      </View>,
+    );
+  }
 
   return (
     <TouchableWithoutFeedback
@@ -263,144 +290,53 @@ const AddGame = ({navigation, route}) => {
             />
           </TouchableOpacity>
           <Text style={styles.title}>Game Details Form</Text>
-          {player1Name !== '' &&
-            player2Name !== '' &&
-            player1GamesWon !== '' &&
-            player2GamesWon !== '' && (
-              <TouchableOpacity
-                style={styles.saveButtonContainer}
-                onPress={() => saveGame()}>
-                <Text style={styles.saveButtonText}>Save</Text>
-              </TouchableOpacity>
-            )}
+          {playerNames.length > 1 && playersGamesWon.length > 1 && (
+            <TouchableOpacity
+              style={styles.saveButtonContainer}
+              onPress={() => saveGame()}>
+              <Text style={styles.saveButtonText}>Save</Text>
+            </TouchableOpacity>
+          )}
         </View>
         <View style={styles.container}>
           <View style={{height: 50}} />
+
           <View style={styles.formContainer}>
-            <View
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
+            {players}
+
+            <TouchableOpacity
+              onPress={() => {
+                setNumberOfPlayers(numberOfPlayers + 1);
+                temp = [...playerNames];
+                temp1 = [...playersGamesWon];
+                temp.push('');
+                temp1.push('');
+                setPlayerNames(temp);
+                setPlayersGamesWon(temp1);
               }}>
-              {player1GamesWon > player2GamesWon && (
-                <Text style={styles.crown}>👑</Text>
-              )}
-              <TextInput
-                ref={player1NameInputRef}
-                style={styles.formInput}
-                placeholder="Player 1 Name"
-                placeholderTextColor={colours.light_text}
-                value={player1Name}
-                onChangeText={text => {
-                  setPlayer1Name(text);
-                  filterPlayerNames(1, text.toLocaleLowerCase());
-                }}
-                onSubmitEditing={() => {
-                  handleSwitchInputRef(1);
-                  setFilteredPlayer1Names([]);
-                }}
-                maxLength={30}
-              />
-              <TextInput
-                ref={player1WinsInputRef}
-                style={styles.formInput}
-                placeholder="Player 1 Games Won"
-                placeholderTextColor={colours.light_text}
-                value={player1GamesWon}
-                onChangeText={number => setPlayer1GamesWon(number)}
-                keyboardType="numeric"
-                onSubmitEditing={() => handleSwitchInputRef(3)}
-                maxLength={4}
-              />
-            </View>
-
-            {filteredPlayer1Names.length > 0 && (
-              <FlatList
-                keyboardShouldPersistTaps={'handled'}
-                style={styles.searchedUsersList}
-                data={filteredPlayer1Names.slice(0, 3)}
-                keyExtractor={item => item}
-                renderItem={({item}) => (
-                  <TouchableOpacity
-                    style={styles.searchedUserContainer}
-                    onPress={() => {
-                      handleSuggestionPress(item, 1);
-                    }}>
-                    <Text style={styles.searchedUserText}>{item}</Text>
-                  </TouchableOpacity>
-                )}
-              />
-            )}
-
-            <Text style={styles.vsText}>VS</Text>
-
-            <View
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
+              <Text>Add player</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setNumberOfPlayers(numberOfPlayers - 1);
+                temp = [...playerNames];
+                temp1 = [...playersGamesWon];
+                temp.pop();
+                temp1.pop();
+                setPlayerNames(temp);
+                setPlayersGamesWon(temp1);
               }}>
-              {player2GamesWon > player1GamesWon && (
-                <Text style={styles.crown}>👑</Text>
-              )}
-              <TextInput
-                ref={player2NameInputRef}
-                style={styles.formInput}
-                placeholder="Player 2 Name"
-                placeholderTextColor={colours.light_text}
-                value={player2Name}
-                onChangeText={text => {
-                  setPlayer2Name(text);
-                  filterPlayerNames(2, text.toLocaleLowerCase());
-                }}
-                onSubmitEditing={() => {
-                  handleSwitchInputRef(2);
-                  setFilteredPlayer2Names([]);
-                }}
-                maxLength={30}
-              />
-              <TextInput
-                ref={player2WinsInputRef}
-                style={styles.formInput}
-                placeholder="Player 2 Games Won"
-                placeholderTextColor={colours.light_text}
-                value={player2GamesWon}
-                onChangeText={number => setPlayer2GamesWon(number)}
-                keyboardType="numeric"
-                onSubmitEditing={() => handleSwitchInputRef(4)}
-                maxLength={4}
-              />
-            </View>
-
-            {filteredPlayer2Names.length > 0 && (
-              <FlatList
-                keyboardShouldPersistTaps={'handled'}
-                style={styles.searchedUsersList}
-                data={filteredPlayer2Names.slice(0, 3)}
-                keyExtractor={item => item}
-                renderItem={({item}) => (
-                  <TouchableOpacity
-                    style={styles.searchedUserContainer}
-                    onPress={() => {
-                      handleSuggestionPress(item, 2);
-                    }}>
-                    <Text style={styles.searchedUserText}>{item}</Text>
-                  </TouchableOpacity>
-                )}
-              />
-            )}
+              <Text>Remove player</Text>
+            </TouchableOpacity>
 
             <View style={styles.addExtraContainer}>
               <TextInput
-                ref={noteInputRef}
                 style={styles.noteInput}
                 placeholder="Add a note"
                 placeholderTextColor={colours.light_text}
                 value={gameNote}
                 onChangeText={string => setGameNote(string)}
                 maxLength={300}
-                onSubmitEditing={() => handleSwitchInputRef(5)}
               />
               <TouchableOpacity
                 style={styles.mediaInput}
@@ -420,7 +356,6 @@ const AddGame = ({navigation, route}) => {
             )}
             {leaderboardPassword && (
               <TextInput
-                ref={passwordInputRef}
                 style={[
                   styles.passwordInput,
                   wrongPassword && {borderWidth: 2, borderColor: 'red'},
